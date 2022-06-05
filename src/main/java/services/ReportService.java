f79fd3097585e952eaffd86e2d7aff1ae3b729e0
saki.kondo <saki.kondo@axas-japan.co.jp>
2022-06-05 23:43:23 +0900

    トップページアクションの作成

--- src/main/java/services/ReportService.java
+++ src/main/java/services/ReportService.java
@@ -36,7 +36,7 @@ public class ReportService extends ServiceBase {
      * @param employee
      * @return 日報データの件数
      */
-    public long contAllMine(EmployeeView employee) {
+    public long countAllMine(EmployeeView employee) {
 
         long count = (long)em.createNamedQuery(JpaConst.Q_REP_COUNT_ALL_MINE, Long.class)
                 .setParameter(JpaConst.JPQL_PARM_EMPLOYEE, EmployeeConverter.toModel(employee))
